e88d95382fd9231cb3cf47f3865a6a89a7d50397
Valentin Zwerschke <vallezw@gmail.com>
2024-08-31 14:24:04 +0200

    Init empty swift proj

---
+++ SheetAble-iPadOS/SheetAble_iPadOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetAble_iPadOSApp.swift
+//  SheetAble-iPadOS
+//
+//  Created by Valentin Zwerschke on 31.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SheetAble_iPadOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
